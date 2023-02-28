282be1cc1f2d0d6427a9b8dc646c2f455782990b
fumbuka1998 <limbufumbuka@gmail.com>
2023-02-28 14:33:57 +0300

    pdf save functionality in todo

--- todo.js
+++ todo.js
@@ -12,4 +12,36 @@ $(document).ready(function(){
     $('ul').on('click','.fa-check', function(){
         $(this).parent('li').toggleClass('checked');
     });
+
+//     $('#mybtn').click(function(){
+//         // Create a new jsPDF object
+//     var doc = new jsPDF();
+
+//      // Get the HTML content of the list
+//     var listContent = $(ul).html();
+
+//   // Convert the HTML content to a canvas object
+//   html2canvas($(ul)[0]).then(function(canvas) {
+//     // Add the canvas to the PDF document
+//     doc.addImage(canvas.toDataURL('image/jpeg'), 'JPEG', 10, 10, 180, 240);
+
+//     // Save the PDF document
+//     doc.save('list.pdf');
+//   });
+//     });
+
+$('#mybtn').click(function(){
+    // alert(22);
+    var doc = new jsPDF();
+    var ul = '#mydoc';
+    var listContent = $(ul).html();
+
+    html2canvas($(ul)[0]).then(function(canvas) {
+            // Add the canvas to the PDF document
+            doc.addImage(canvas.toDataURL('image/jpeg'), 'JPEG', 10, 10, 180, 240);
+        
+            // Save the PDF document
+            doc.save('list.pdf');
+          });
+});
 });
